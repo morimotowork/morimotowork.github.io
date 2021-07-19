09fbb564d3b1ce1bcdee57c89f13574e45dc86c8
juniormorimoto <morimotojunior@gmail.com>
2021-07-19 14:24:20 -0300

    Portfolio v1

---
+++ script/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    if (window.matchMedia( "(min-width: 768px)" ).matches) {
+
+        $("ul#projects li").mousemove(function(){
+
+            if(event.pageY > $(window).outerHeight() - $('img', this).outerHeight()){
+                $('img', this).css({"top": event.pageY-$('img', this).outerHeight()-2, "left": event.pageX+2, "display": "block"}); 
+            } else {
+                $('img', this).css({"top": event.pageY+2, "left": event.pageX+2, "display": "block"}); 
+            }
+
+            $(this).mouseleave(function(){
+                $('img', this).hide(); 
+            });
+
+        });
+
+        $(function(){
+            $( "img.movel" ).draggable();
+        });
+
+    }
+
+    var ano = new Date().getFullYear();
+
+    $( "header#index h1" ).text($( "header#index" ).text().replace("2021", ano));
+    $( "footer").find("h1:last").text($( "footer").find("h1:last").text().replace("2021", ano));
+
+});
